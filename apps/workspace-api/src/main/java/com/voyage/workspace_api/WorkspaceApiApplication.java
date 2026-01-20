99dada5dcff211d39639edab9a4d3c9048686fdb
Domonkos <162434141+domonkosszer@users.noreply.github.com>
2026-01-20 16:42:42 +0100

    Workspace-API

---
+++ apps/workspace-api/src/main/java/com/voyage/workspace_api/WorkspaceApiApplication.java
@@ -0,0 +1,13 @@
+package com.voyage.workspace_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkspaceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkspaceApiApplication.class, args);
+	}
+
+}
---
+++ apps/workspace-api/src/test/java/com/voyage/workspace_api/WorkspaceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voyage.workspace_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkspaceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
